6ba1ca9d7b3c88eb5e4115b45a1d118288be6855
zhangxin <862257810@qq.com>
2021-04-15 15:43:23 +0800

    fix:修复用户dao层、service的处理行为

--- service/loginService.ts
+++ service/loginService.ts
@@ -20,33 +20,31 @@ import { LoginParams } from '../types/login';
 function login(params: LoginParams) {
   const { phone, password } = params;
 
-  return new Promise((resolve, reject) => {
-    UserDao.findUser({ phone }).then(
-      (data: any) => {
-        // 比对密码
-        const flag = bcrypt.compareSync(password, data.password);
+  return new Promise(async (resolve, reject) => {
+    const data = await UserDao.findUser({ phone });
+    if (!data) {
+      reject('登录失败,账号或密码错误');
+    } else {
+      // 比对密码
+      const flag = bcrypt.compareSync(password, data.password);
 
-        if (flag) {
-          // 登录成功, 生成token
-          const { secret, expiresIn } = jwt_config;
-          const token: string = jwt.sign({ phone }, secret, {
-            expiresIn,
-          });
-          //data是mongoose返回的,不是一个JS对象，不能对其操作,故需要转为JS对象
-          let result: Data = JSON.parse(JSON.stringify(data));
-          // Beaeer后一定要有一个空格
-          result.token = 'Bearer ' + token;
+      if (flag) {
+        // 登录成功, 生成token
+        const { secret, expiresIn } = jwt_config;
+        const token: string = jwt.sign({ phone }, secret, {
+          expiresIn,
+        });
+        //data是mongoose返回的,不是一个JS对象，不能对其操作,故需要转为JS对象
+        let result: Data = JSON.parse(JSON.stringify(data));
+        // Beaeer后一定要有一个空格
+        result.token = 'Bearer ' + token;
 
-          resolve(result);
-        } else {
-          // promise中thorw 在后面的catch中可以拿到错误信息
-          reject('登录失败,账号或密码错误');
-        }
-      },
-      () => {
+        resolve(result);
+      } else {
+        // promise中thorw 在后面的catch中可以拿到错误信息
         reject('登录失败,账号或密码错误');
       }
-    );
+    }
   });
 }
 export default {
